90d3004fcaa160387cb1d24869ead9c72f0d5299
Fabio Pereira <fabiuhp@msn.com>
2023-09-25 19:04:14 -0300

    first commit

---
+++ src/main/java/com/fabiopereira/ubertest/UberTestApplication.java
@@ -0,0 +1,13 @@
+package com.fabiopereira.ubertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabiopereira/ubertest/UberTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiopereira.ubertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
